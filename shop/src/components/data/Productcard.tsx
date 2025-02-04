2638c4259dd0e3a306af0bdf8d919092c08708b9
1221solomon-tadesse <solomontadesse480@gmail.com>
2025-02-04 13:27:49 +0300

    updated

---
+++ shop/src/components/data/Productcard.tsx
@@ -0,0 +1,9 @@
+import img1 from "@/app/asset/bag.png"
+import img2 from '@/app/asset/shoe.png'
+import img3 from "@/app/asset/150577_BROWN_9 2.png"
+const Card={
+img1,
+img2,
+img3 
+}
+export default Card
--- shop/src/components/presentation/Home.tsx
+++ shop/src/components/presentation/Home.tsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <main className="bg-bg">
       <div className="grid grid-cols-1 lg:grid-cols-3  mx-auto px-4">
-        <div className="col-span-1 flex justify-center items-center py-24">
-          <h1 className="text-white text-128 font-alfa leading-175 text-center">
+        <div className=" col-span-1 flex justify-center items-center py-24">
+          <h1 className="text-white text-128 font-alfa leading-175  text-end" >
             Gabeya
           </h1>
         </div>
@@ -16,7 +16,7 @@ const Home = () => {
         </div>
 
         <div className="flex flex-col justify-between text-white pt-24 w-full h-full">
-          <h1 className="leading-3rem text-20 text-center lg:text-left">
+          <h1 className="leading-3rem text-20  lg:text- text-start w-2/3">
             Discover a world of convenience with our user-friendly platform,
             curated collections, and exceptional customer service
           </h1>
@@ -24,7 +24,7 @@ const Home = () => {
             Shop
           </h1>
         </div>
-        <div className="flex justify-center items-center lg:col-span-1 row-span-3 align-top">
+        <div className="flex justify-center items-center lg:col-span-1 row-span-3 align-top mb-24">
           <Image src={image.imag2} alt="shoe" />
         </div>
         <div className="  lg:col-span-1 row-span-3 justify-center pt-24 ">
